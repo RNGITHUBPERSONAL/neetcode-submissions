0a0ed90fc4723cfb4cc803ae8d79e7d428fb9f08
RN_GIT_PERSONAL <104487999+RNGITHUBPERSONAL@users.noreply.github.com>
2026-06-17 20:38:48 +0530

    Add: minimum-stack - submission-3

---
+++ Data Structures & Algorithms/minimum-stack/submission-3.java
@@ -0,0 +1,34 @@
+class MinStack {
+    Stack<Integer> stack = null;
+    Stack<Integer> minStack = null;
+   
+   
+    public MinStack() {
+        stack = new Stack<>();
+        minStack=new  Stack<>();
+    }
+
+    public void push(int val) {
+        stack.push(val);
+
+        if(minStack.isEmpty() || minStack.peek() >=val) {
+            minStack.push(val);
+        }
+    }
+
+    public void pop() {
+        int val = stack.pop();
+
+        if (val == minStack.peek()) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}
