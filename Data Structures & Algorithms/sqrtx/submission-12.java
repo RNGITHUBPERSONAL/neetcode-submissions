c2321bc44b6ace281beca2599b93ac750cb834fc
RN_GIT_PERSONAL <104487999+RNGITHUBPERSONAL@users.noreply.github.com>
2026-06-25 07:57:24 +0530

    Add: sqrtx - submission-12

---
+++ Data Structures & Algorithms/sqrtx/submission-12.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int mySqrt(int x) {
+     int start =1;int end=x; 
+
+       while(start<=end){
+           int mid= start+(end-start)/2;
+     long square = (long) mid * mid;
+    long nextSquare = (long) (mid + 1) * (mid + 1);
+
+           if( square == x){
+               return mid;
+           } else if (square>x) {
+             end=mid;
+
+           }else if (square<x && (long) nextSquare>x ) {
+              return mid;
+           }
+
+           else {
+               start=mid+1;
+           }
+       }
+       return 0;
+    }
+}
