0014bc230470e6a7a5cda66a887d550536dce718
RN_GIT_PERSONAL <104487999+RNGITHUBPERSONAL@users.noreply.github.com>
2026-07-10 21:55:00 +0530

    Add: max-consecutive-ones-iii - submission-1

---
+++ Data Structures & Algorithms/max-consecutive-ones-iii/submission-1.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int longestOnes(int[] nums, int k) {
+       int max = Integer.MIN_VALUE;
+        int right = 0;
+        int left = 0;
+        int zeroCount = 0;
+
+
+        while (right < nums.length) {
+           if(nums[right] == 0) {
+               zeroCount++;
+           }
+
+           while(zeroCount>k ){
+              if(nums[left]==0){
+  zeroCount--;
+              }
+             
+               left++;;
+
+           }
+            max = Math.max(max, (right - left) + 1);
+            right++;
+
+
+
+        }
+
+
+       
+      
+return max;
+    }
+}
