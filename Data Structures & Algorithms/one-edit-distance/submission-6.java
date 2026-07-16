e2031b2f0c68f04add03c58464b2c99c6527882a
RN_GIT_PERSONAL <104487999+RNGITHUBPERSONAL@users.noreply.github.com>
2026-07-16 16:12:09 +0530

    Add: one-edit-distance - submission-6

---
+++ Data Structures & Algorithms/one-edit-distance/submission-6.java
@@ -0,0 +1,59 @@
+class Solution {
+  public boolean isOneEditDistance(String s, String t) {
+    int sLength = s.length();
+    int tLength = t.length();
+    if (Math.abs(s.length() - t.length()) > 1) {
+      return false;
+    }
+
+    // Case 1: Replace exactly one character
+    if (sLength == tLength) {
+      int count = 0;
+
+      for (int i = 0; i < sLength; i++) {
+        if (s.charAt(i) != t.charAt(i)) {
+          count++;
+
+          if (count > 1) {
+            return false;
+          }
+        }
+      }
+
+      return count == 1;
+    }
+// s = "abc"
+// t = "abxc"
+
+
+    int count = 0;
+    int start = 0;
+    int fast = 0;
+    //why we need alter becase insted of delte we can just reverse so it can handel it in addit like:// s = "abxc"
+// t = "abc"
+if(s.length()>t.length()){
+String temp=s;
+s=t;
+t=temp;
+
+}
+
+    while (start < s.length() && fast < t.length()) {
+  
+
+      if (s.charAt(start) == t.charAt(fast)) {
+        start++;
+        fast++;
+      } else {
+        count++;
+        fast++;
+
+        if (count > 1) {
+          return false;
+        }
+      }
+    }
+
+    return true;
+  }
+}
