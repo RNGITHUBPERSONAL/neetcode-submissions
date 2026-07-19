886c5dfd17f1833312d61cf8640f660318ca2ff3
RN_GIT_PERSONAL <104487999+RNGITHUBPERSONAL@users.noreply.github.com>
2026-07-19 14:28:30 +0530

    Add: remove-duplicates-from-sorted-array-ii - submission-0

---
+++ Data Structures & Algorithms/remove-duplicates-from-sorted-array-ii/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+     int j=1;int count=1;
+
+        for(int i=1;i<nums.length;i++){
+            if(nums[i-1]==nums[i]){
+                count++;
+
+            }else {
+                count=1;
+            }
+            if(count<=2){
+                nums[j]=nums[i];
+                j=j+1;
+            }
+        }
+        return j;   
+    }
+}
