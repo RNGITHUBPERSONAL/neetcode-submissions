c1c133f5246dd3b0716bde21d9f15c4dfe4c5481
RN_GIT_PERSONAL <104487999+RNGITHUBPERSONAL@users.noreply.github.com>
2026-07-24 20:21:28 +0530

    Add: find-k-closest-elements - submission-0

---
+++ Data Structures & Algorithms/find-k-closest-elements/submission-0.java
@@ -0,0 +1,36 @@
+class Solution {
+    public List<Integer> findClosestElements(int[] arr, int k, int x) {
+   
+PriorityQueue<int[]> priorityQueue=new PriorityQueue<>(
+
+        (a,b)-> {
+            if (a[0] != b[0]) {
+                return Integer.compare(b[0], a[0]);
+            }
+            return Integer.compare(b[1], a[1]);
+
+        }
+);
+
+
+for(int num:arr){
+
+    priorityQueue.offer(new int[]{Math.abs(num-x),num});
+    if(priorityQueue.size()>k){
+        priorityQueue.poll();
+    }
+
+}
+
+        ArrayList<Integer> arrayList= new ArrayList<>();
+
+for(int []num:priorityQueue){
+    int val=num[1];
+
+    arrayList.add(val);
+}
+Collections.sort(arrayList);
+
+        return arrayList;
+    }
+}
