65ee57c0c34610132b54a51c458d1de610e5e0af
RN_GIT_PERSONAL <104487999+RNGITHUBPERSONAL@users.noreply.github.com>
2026-09-08 14:08:26 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+  Arrays.sort(strs);
+
+String first = strs[0];
+String last = strs[strs.length - 1];
+
+int i = 0;
+
+for (i = 0; i < first.length(); i++) {
+
+    char ch1 = first.charAt(i);
+    char ch2 = last.charAt(i);
+
+    if (ch1 != ch2) {
+        break;
+    }
+}
+
+return first.substring(0, i);
+    }
+}
