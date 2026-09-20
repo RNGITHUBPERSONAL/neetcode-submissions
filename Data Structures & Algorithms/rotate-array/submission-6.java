f41bad3f214feb359fd7bd2fe251e968cc5033f1
RN_GIT_PERSONAL <104487999+RNGITHUBPERSONAL@users.noreply.github.com>
2026-09-20 21:21:20 +0530

    Add: rotate-array - submission-6

---
+++ Data Structures & Algorithms/rotate-array/submission-6.java
@@ -0,0 +1,23 @@
+class Solution {
+    public void rotate(int[] nums, int k) {
+          k = k % nums.length;
+        reverseArray(nums, 0, nums.length - 1);
+        reverseArray(nums, 0, k - 1);
+
+        reverseArray(nums, k, nums.length - 1);
+    }
+}
+private static void reverseArray(int[] nums, int i, int k) {
+    while (i < k) {
+        int num = nums[i];
+        nums[i] = nums[k];
+        nums[k] = num;
+        i++;
+        k--;
+    }
+}
+// k = k % nums.length;
+
+// reverseArray(nums, 0, k - 1);
+// reverseArray(nums, k, nums.length - 1);
+// reverseArray(nums, 0, nums.length - 1);
