8df306a9a8574e83b5635a083cd21faf1693f95d
RN_GIT_PERSONAL <104487999+RNGITHUBPERSONAL@users.noreply.github.com>
2026-09-22 22:36:36 +0530

    Add: contains-duplicate-ii - submission-2

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-2.java
@@ -0,0 +1,19 @@
+class Solution {
+  public boolean containsNearbyDuplicate(int[] nums, int k) {
+    HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+ 
+
+    for (int i = 0; i < nums.length; i++) {
+      if (hashMap.containsKey(nums[i])) {
+        int diff = Math.abs(hashMap.get(nums[i]) - i);
+        if (diff <= k) {
+          return true;
+        }
+      }
+      hashMap.put(nums[i], i);
+    }
+
+    return false;
+  }
+}
